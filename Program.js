2dac59bcd5f0397a1ae24526cde96c0add3da629
Monjurul Alam <monjurulalam190629@gmail.com>
2025-02-17 22:18:38 +0600

    first commit

---
+++ Program.js
@@ -0,0 +1,8 @@
+"use strict";
+const concate = (a, b) => {
+    return a + b;
+};
+const ge = 2;
+console.log(ge);
+console.log(concate('Hello ', 'World'));
+console.log(concate("10", "5"));
---
+++ Program.ts
@@ -0,0 +1,9 @@
+const concate = (a:string, b:string) => {
+    return a + b;
+}
+
+const ge: number = 2;
+console.log(ge)
+
+console.log(concate('Hello ', 'World'))
+console.log(concate("10", "5"))
